29e5d289de18fa958c4a7b7cbcac5fe40badb716
Jose Luis Falla <jose.luis.falla@gmail.com>
2019-10-09 12:19:03 -0500

    Update Optimize.m

--- Optimize.m
+++ Optimize.m
@@ -1,26 +1,26 @@
-function [fidelity_current] = Optimize(d, epsilon, eta, C, Permute, N, px, py, pz, batch_size, iters, state)
+function [fidelity_update, eta] = Optimize(d, epsilon, eta, C, Permute, N, px, py, pz, batch_size, iters, state)
 
-fidelity_current = zeros(iters, 1);
+fidelity_update = zeros(iters, 1);
 
 for i = 1:iters
 
     fidelity_previous = Fidelity(d, C, Permute, N, px, py, pz, batch_size, state);
 
-    dfdc = FiniteDifference(d, epsilon, C, Permute, N, px, py, pz, batch_size, state);
+    df_dc = FiniteDifference(d, epsilon, C, Permute, N, px, py, pz, batch_size, state);
 
     C = permute(C, [3 1 2]);
 
-    C_update = C + eta * dfdc;
+    C_update = C + eta * df_dc;
 
     C_update = permute(C_update, [3 2 1]);
 
-    fidelity_current(i) = Fidelity(d, C_update, Permute, N, px, py, pz, batch_size, state);
+    fidelity_update(i) = Fidelity(d, C_update, Permute, N, px, py, pz, batch_size, state);
 
-    if (fidelity_current - fidelity_previous) > 0
+    if (fidelity_update - fidelity_previous) > 0
 
         eta = eta + (0.05 * eta);
 
-    elseif (fidelity_current - fidelity_previous) < 0
+    elseif (fidelity_update - fidelity_previous) < 0
 
         eta = eta - (0.5 * eta);
 
